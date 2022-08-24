bc968e018f01a1575cf6f8c6cf869c9e38a8efd6
Jack Sawyer <84543199+JackSawyerWATX@users.noreply.github.com>
2022-08-23 20:23:34 -0700

    first commit

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    String a = "5";
+    String b = "6";
+    String c = "2";
+    String d = "1";
+    String e = "9";
+    String f = "2";
+    String g = "-";
+    String h = "8";
+  
+    public static void main(String[] args) {
+      Main myObj1 = new Main();
+      Main myObj2 = new Main();
+      Main myObj3 = new Main();
+      Main myObj4 = new Main();
+      Main myObj5 = new Main();
+      Main myObj6 = new Main();
+      Main myObj7 = new Main();
+      Main myObj8 = new Main();
+      System.out.print(myObj6.f);
+      System.out.print(myObj7.h);
+      System.out.print(myObj8.d);
+      System.out.print(myObj1.g);
+      System.out.print(myObj1.a);
+      System.out.print(myObj2.b);
+      System.out.print(myObj1.a);
+      System.out.print(myObj1.g);
+      System.out.print(myObj3.c);
+      System.out.print(myObj5.e);
+      System.out.print(myObj4.d);
+      System.out.print(myObj5.e);
+    }
+  }
+  
